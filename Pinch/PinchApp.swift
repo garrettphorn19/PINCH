83a205639cc26b1efc489b9b650bbbe3eb053971
Garrett Horn <garrett@sbfmail.com>
2022-08-11 06:31:40 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Garrett Horn on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
